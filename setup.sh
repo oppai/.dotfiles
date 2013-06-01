b6390fc7695db8c26db6b6c43c0aac9a3c7213d3
kodam <hotsoup.h@gmail.com>
2013-06-01 19:06:29 +0900

    add gvim setting

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DOT_FILES=( .bash_profile .screenrc .vimrc )
+DOT_FILES=( .bash_profile .screenrc .vimrc .gvimrc)
 
-#install neobundle
+:q#install neobundle
 if [ ! -d ~/.vim/neobundle.vim.git ]; then
 	git clone https://github.com/Shougo/neobundle.vim ~/.vim/neobundle.vim.git
 fi
