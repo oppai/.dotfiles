8cf9b3f46d01f4b8296fb8a285ba97ec3116fdb6
kodam <hotsoup.h@gmail.com>
2015-04-02 17:40:02 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DOT_FILES=( .bash_profile .gitconfig .screenrc .vimrc .gvimrc .emacs .git-completion.bash
-            .tmux.conf .gvimrc .zprofile)
+DOT_FILES=( .bash_profile .gitconfig .screenrc .vimrc .gvimrc .xvimrc .emacs .git-completion.bash
+            .tmux.conf .gvimrc .zprofile .gitignore)
 
 #install neobundle
 if [ ! -d ~/.vim/neobundle.vim.git ]; then
