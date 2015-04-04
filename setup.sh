043b851724e472949727fb7192771454471df1e5
hiroaki.murayama <hiroaki.murayama@mixi.co.jp>
2015-04-04 19:49:45 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -9,15 +9,15 @@ if [ ! -d ~/.vim/neobundle.vim.git ]; then
 fi
 
 #copy vim script for dont use neobundle
-for file in `ls ~/.dotfiles.mixi/.vim/plugin/`;
+for file in `ls ~/.dotfiles/.vim/plugin/`;
 do
-	ln -s ~/{.dotfiles.mixi/,}.vim/plugin/$file;
+	ln -s ~/{.dotfiles/,}.vim/plugin/$file;
 done
 
 #copy dotfiles
 for file in ${DOT_FILES[@]};
 do
-    ln -s $HOME/.dotfiles.mixi/$file $HOME/$file;
+    ln -s $HOME/.dotfiles/$file $HOME/$file;
 done
 
 echo "Complete setup.sh"
